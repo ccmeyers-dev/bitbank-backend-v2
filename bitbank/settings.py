be4fe89d4cd1f4abea447526df9ecba8b927aaba
ccmeyers-dev <charlotteccmeyers@outlook.com>
2020-09-19 19:54:14 +0100

    fresh

--- bitbank/settings.py
+++ bitbank/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = bool(strtobool(os.getenv('DEBUG', 'False')))
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
