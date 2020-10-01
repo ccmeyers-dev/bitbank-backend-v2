bbe28ef5660e0c8d542f249cfb2cc4007bd66948
ccmeyers-dev <charlotteccmeyers@outlook.com>
2020-10-01 02:31:37 +0100

    adjust token life span

--- bitbank/settings.py
+++ bitbank/settings.py
@@ -71,7 +71,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
     'ROTATE_REFRESH_TOKEN': True,
     'BLACKLIST_AFTER_ROTATION': True,
