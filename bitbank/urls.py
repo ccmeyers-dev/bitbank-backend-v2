d3de56d1b1afb1aa23bd286ab50b182da5309c75
ccmeyers-dev <charlotteccmeyers@outlook.com>
2020-10-20 17:15:48 +0100

    adding prev homepage to backend

--- bitbank/urls.py
+++ bitbank/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
-    path('ccmeyers100/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name="home.html")),
     path('api/auth/', include('authentication.urls')),
     path('api/users/', include('broker.urls'))
 ]
